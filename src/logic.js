42a73e173cbf43ebd82a42ac6d94b1caf0ddb704
Serj <email>
2023-05-26 14:54:19 +0300

    one change

--- src/logic.js
+++ src/logic.js
@@ -1,27 +1,33 @@
-import { openai } from './openai.js'
+// import { openai } from './openai.js'
+// import { textConverter } from './text.js'
 
-export const INITIAL_SESSION = {
-  messages: [],
-}
+// export const INITIAL_SESSION = {
+//   messages: [],
+// }
 
-export async function initCommand(ctx) {
-  ctx.session = { ...INITIAL_SESSION }
-  await ctx.reply('Жду вашего голосового или текстового сообщения ')
-}
+// export async function initCommand(ctx) {
+//   ctx.session = { ...INITIAL_SESSION }
+//   await ctx.reply('Жду вашего голосового или текстового сообщения ')
+// }
 
-export async function processTextToChat(ctx, content) {
-  try {
-    ctx.session.messages.push({ role: openai.roles.USER, content })
+// export async function processTextToChat(ctx, content) {
+//   try {
+//     ctx.session.messages.push({ role: openai.roles.USER, content })
 
-    const response = await openai.chat(ctx.session.messages)
+//     const response = await openai.chat(ctx.session.messages)
 
-    ctx.session.messages.push({
-      role: openai.roles.ASSISTANT,
-      content: rsponse.content,
-    })
+//     ctx.session.messages.push({
+//       role: openai.roles.ASSISTANT,
+//       content: rsponse.content,
+//     })
 
-    await ctx.reply(response.content)
-  } catch (e) {
-    console.log('Error while processing text to gpt', e.message)
-  }
-}
+//     const source = await textConverter.textToSpeech(response.content)
+
+//     await ctx.sendAudio({ source }),
+//       { title: 'Ответ от ассистента', performer: 'ChatGPT' }
+
+    //// await ctx.reply(response.content)
+//   } catch (e) {
+//     console.log('Error while processing text to gpt', e.message)
+//   }
+// }
